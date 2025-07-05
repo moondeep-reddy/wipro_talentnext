4a1b0b75ec7bc26db9299bd18c205ac670597ebc
moondeep-reddy <moondeep0420@gmail.com>
2025-07-05 11:25:51 +0530

    Update problem9.java

--- problem9.java
+++ problem9.java
@@ -1,5 +1,16 @@
 package wipro;
-
+import java.util.Scanner;
 public class problem9 {
-
+	public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Enter the character");
+	char ch = sc.next().charAt(0);
+	if(Character.isLetter(ch)) {
+		System.out.println("Alphabet");
+	}else if(Character.isDigit(ch)) {
+		System.out.println("Digit");
+	}else {
+		System.out.println("Special symbol");
+	}
+  }
 }
